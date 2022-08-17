8667b4ffa588fc575dde80bf4cdb9b22125708a5
ritika309 <ritikasaini123456@gmail.com>
2022-08-17 19:11:48 +0530

    2

--- index.js
+++ index.js
@@ -2,16 +2,24 @@
 
 var express = require('express')
 var ejs = require('ejs')
+var bodyParser = require('body-parser');
 
 var app = express();
 
+//tells the program to use public folder
 app.use(express.static('public'));
+
+//use ejs file as view engine
 app.set('view engine','ejs');
 
-app. listen(8080);
-
+app.listen(8080);
 //localhost:8080
-app.get('/',function(req,res){
-    res.render('pages/index'); ///views decalred already 9
 
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+app.get('/',function(req,res){
+    res.render('pages/index'); ///views is already known node, it is for html + don't use .ejs extension
+ 
 });
