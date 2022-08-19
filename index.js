d983cd1f851ad4288c75cb1f2e70645f444a11bd
ritika309 <ritikasaini123456@gmail.com>
2022-08-20 00:55:33 +0530

    products

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //this file will serve as the connector point for all files of our project
 
-var express = require('express')
-var ejs = require('ejs')
+var express = require('express');
+var ejs = require('ejs');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+//create connection
+ mysql.createConnection({
+    host:"127.0.0.1",
+    user:"root",
+    password:"",
+    database:"ecommerce"
+})
+//
 
 var app = express();
 
@@ -17,9 +27,16 @@ app.listen(8080);
 
 app.use(bodyParser.urlencoded({
     extended:true
-}))
+}));
 
 app.get('/',function(req,res){
-    res.render('pages/index'); ///views is already known node, it is for html + don't use .ejs extension
+    var con = mysql.createConnection({
+        host:"127.0.0.1",
+        user:"root",
+        password:"",
+        database:"ecommerce"
+    })
+    con.query("SELECT * FROM products",(err,result)=>{res.render('pages/index',{result:result}); })
+    ///views is already known node, it is for html + don't use .ejs extension
  
 });
